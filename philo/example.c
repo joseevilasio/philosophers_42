f42c4b569071023accf2cbab35b396917493ec61
Jose Junior <jose_evilasio@outlook.com>
2024-12-31 17:04:30 +0000

    initial setup

---
+++ philo/example.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joneves- <joneves-@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/31 11:06:23 by joneves-          #+#    #+#             */
+/*   Updated: 2024/12/31 17:02:10 by joneves-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "philo.h"
+
+long	mails = 0;
+pthread_mutex_t	mutex;
+
+void	*routine()
+{
+	int	i;
+
+	i = 0;
+	while (i < 1000000)
+	{
+		pthread_mutex_lock(&mutex);
+		mails++;
+		pthread_mutex_unlock(&mutex);
+		i++;
+	}
+	return (NULL);
+}
+
+int	main(/* int argc, char **argv */)
+{
+	pthread_t	thread[10];
+	int			i;
+
+	i = 0;
+	pthread_mutex_init(&mutex, NULL);
+
+	while (i < 10)
+	{
+		if (pthread_create(thread + i, NULL, &routine, NULL) != 0)
+			return (printf("Error\n"), 1);
+		printf("Thread %d has started\n", i);
+		i++;
+	}
+	i = 0;
+	while (i < 10)
+	{
+		if (pthread_join(thread[i], NULL) != 0)
+		return (printf("Error\n"), 1);
+		printf("Thread %d has finished\n", i);
+		i++;
+	}
+	pthread_mutex_destroy(&mutex);
+	printf("Emails: %ld\n", mails);
+	return (0);
+}
---
+++ philo/philo.h
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joneves- <joneves-@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/31 11:06:51 by joneves-          #+#    #+#             */
+/*   Updated: 2024/12/31 13:18:07 by joneves-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+/* External functs.
+
+string.h
+	menset
+
+stdio.h
+	printf
+
+stdlib.h
+	malloc | free
+
+unistd.h
+	write | usleep
+
+sys/time.h
+	gettimeofday
+
+pthread.h
+	pthread_create | pthread_detach | pthread_join | pthread_mutex_init
+	pthread_mutex_destroy | pthread_mutex_lock | pthread_mutex_unlock
+*/
+
+#endif //PHILO_H
