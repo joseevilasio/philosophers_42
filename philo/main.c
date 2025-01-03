3b9292b3ddbe85543bb93f5b4edc79cce6017844
Jose Junior <jose_evilasio@outlook.com>
2025-01-03 19:31:58 +0000

    new example

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: joneves- <joneves-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/31 11:06:23 by joneves-          #+#    #+#             */
-/*   Updated: 2025/01/02 18:14:23 by joneves-         ###   ########.fr       */
+/*   Updated: 2025/01/03 19:28:25 by joneves-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,11 @@ int	main(int argc, char **argv)
 	//	return (2);
 	// if (usleep(LONG_MAX) == -1)
 	// 	printf("error usleep\n");
+	printf("%lu\n", ft_get_time());
+	
+	usleep(ft_atol(argv[1]));
+
+	printf("%lu\n", ft_get_time());
 	printf("Starting... \n");
 	return (0);
 }
