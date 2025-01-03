50c599d84b65f0fc2abdd988bf1ab7694c1b5c32
Jose Junior <jose_evilasio@outlook.com>
2025-01-03 22:48:14 +0000

    fixed logic

--- philo/ft_parser.c
+++ philo/ft_parser.c
@@ -6,13 +6,13 @@
 /*   By: joneves- <joneves-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/31 17:17:58 by joneves-          #+#    #+#             */
-/*   Updated: 2025/01/03 18:22:28 by joneves-         ###   ########.fr       */
+/*   Updated: 2025/01/03 22:21:38 by joneves-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static int	ft_valid_argument(int argc, char *argv)
+static int	ft_valid_argument(char *argv)
 {
 	int		i;
 	long	nbr;
@@ -28,16 +28,8 @@ static int	ft_valid_argument(int argc, char *argv)
 		i++;
 	}
 	nbr = ft_atol(argv);
-	if (argc > 1 && argc < 5)
-	{
-		if (nbr < 1 || nbr > UINT_MAX)
-			return (printf("Error: %s: numeric argument required\n", argv), -1);
-	}
-	else
-	{
-		if (nbr < 1 || nbr > INT_MAX)
-			return (printf("Error: %s: numeric argument required\n", argv), -1);
-	}
+	if (nbr < 1 || nbr > INT_MAX)
+		return (printf("Error: %s: numeric argument required\n", argv), -1);
 	return (0);
 }
 
@@ -52,7 +44,7 @@ int	ft_parser(int argc, char **argv)
 		return (printf("Error: too many arguments\n"), -1);
 	while (argv[i])
 	{
-		if (ft_valid_argument(i, argv[i]) != 0)
+		if (ft_valid_argument(argv[i]) != 0)
 			return (-1);
 		i++;
 	}
