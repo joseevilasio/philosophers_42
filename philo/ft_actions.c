939b5e58976f3190e385031cb94301e83892f32f
Jose Junior <jose_evilasio@outlook.com>
2025-01-08 18:17:08 +0000

    added mutex

--- philo/ft_actions.c
+++ philo/ft_actions.c
@@ -6,7 +6,7 @@
 /*   By: joneves- <joneves-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 21:16:37 by joneves-          #+#    #+#             */
-/*   Updated: 2025/01/05 21:34:50 by joneves-         ###   ########.fr       */
+/*   Updated: 2025/01/07 23:02:54 by joneves-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,23 @@
 void	ft_eat(t_philo *philo)
 {
 	printf(MSG_EAT, ft_get_time(), philo->id);
-	usleep(philo->time_to_eat * 1000);
+	usleep((philo->table)->time_to_eat * 1000);
+	philo->fork = 0; //without a fork | maybe mutex
+	pthread_mutex_lock(&philo->table->mutex_meal);
 	philo->meals++;
-	pthread_mutex_lock(&(philo->table)->mutex);
 	philo->last_meal = ft_get_time();
-	pthread_mutex_unlock(&(philo->table)->mutex);
-	if (philo->meals == philo->meal_goal)
+	if (philo->meal_goal != 0)
 	{
-		pthread_mutex_lock(&(philo->table)->mutex);
-		philo->reached = 1;
-		pthread_mutex_unlock(&(philo->table)->mutex);
+		if (philo->meals == philo->meal_goal)
+			philo->table->meal_goal_each++;
 	}
+	pthread_mutex_unlock(&philo->table->mutex_meal);
 }
 
 void	ft_sleep(t_philo *philo)
 {
 	printf(MSG_SLEEP, ft_get_time(), philo->id);
-	usleep(philo->time_to_sleep * 1000);
+	usleep(philo->table->time_to_sleep * 1000);
 }
 
 void	ft_think(t_philo *philo)
@@ -46,5 +46,17 @@ void	ft_die(t_philo *philo)
 
 void	ft_take_fork(t_philo *philo)
 {
+	int	next;
+	
+	next = philo->id + 1;
+	//mutex lock
+	philo->fork == 1; // try take a fork
+	if (!philo->table->philos[next])
+		next = 0;
+	(philo->table->philos[next])->fork = 1;
+	//eu tenho um garfo
+	// ft eat
+	//unlock
+	
 	printf(MSG_TAKE_FORK, ft_get_time(), philo->id);
 }
